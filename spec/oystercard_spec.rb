034085eb08d5087df652a8d8f9894fec473af857
dan-cl <djclissold@yahoo.co.uk>
2016-07-12 14:29:13 +0100

    7. Deducting money

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -26,4 +26,13 @@ describe Oystercard do
       end
 
   end
+
+  describe '#deduct' do
+    it {is_expected.to respond_to(:deduct).with(1).argument}
+
+    it 'reduces the balance' do
+      subject.top_up(20)
+      expect {subject.deduct(5)}.to change{subject.balance}.by -5
+    end
+  end
 end
