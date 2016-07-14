d95641a7dfdbac60581869ce9e9571cf792b0ccf
Georgia Mills <georgee_123@hotmail.com>
2016-07-14 13:51:43 +0100

    stuff again

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -10,8 +10,7 @@ class OysterCard
 
   def initialize(balance = 0)
     @balance = balance
-    @journey_log = []
-
+    @journeys = []
   end
 
   def top_up amount
@@ -21,27 +20,23 @@ class OysterCard
 
   def touch_in(station)
     fail min_balance_message if min_balance?
-    new_journey.begining_station(station)
-    @journey_log << journey
+    journey = Journey.new
+    journey.begining_station(station)
+    @journeys << journey
     station
   end
 
   def in_journey?
-    @journey_log[-1].in_journey?
+    @journeys[-1].in_journey?
 
   end
 
   def touch_out(station)
-    if new_journey.empty?
-      new_journey
     #journey2 = Journey.new if @journeys.nil?
     deduct MINIMUM_FARE
-    @journey_log.last.final_station(station)
+    @journeys.last.final_station(station)
   end
 
-  def new_journey
-    journey = Journey.new
-  end
 
   def min_balance_message
     "Card has insufficient balance"
